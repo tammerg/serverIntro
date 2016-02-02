1476fce321433b0078c525ec36821de2b27ace64
tammerg <tg385@scarletmail.rutgers.edu>
2016-02-02 18:51:25 -0500

    my first server is up and running!

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+var PORT = 8080;
+
+function handleRequest(request, response){
+  response.end("It works!! Path: " + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+  console.log("Server is listening on http://localhost:%s", PORT);
+});
