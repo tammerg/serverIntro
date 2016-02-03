b9bcf89cb269aef1a87808942c5e535fa526ef36
tammerg <tg385@scarletmail.rutgers.edu>
2016-02-02 19:44:54 -0500

    got two servers and running simultaneously on two seperate ports!

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var http = require("http");
 
-var PORT = 8080;
+var port1 = 7000;
+var port2 = 7500;
 
-function handleRequest(request, response){
-  response.end("It works!! Path: " + request.url);
+function nicehandleRequest(request, response){
+  response.end("You are wonderful");
+}
+function meanhandleRequest(request, response){
+  response.end("You are an ass");
 }
 
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(nicehandleRequest);
+var server2 = http.createServer(meanhandleRequest);
 
-server.listen(PORT, function(){
-  console.log("Server is listening on http://localhost:%s", PORT);
+server1.listen(port1, function(){
+  console.log("Server is listening on http://localhost:%s", port1);
+});
+server2.listen(port2, function(){
+  console.log("Server is listening on http://localhost:%s", port2);
 });
